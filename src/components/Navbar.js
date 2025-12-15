de09749f6b5dadfa1c5c939343da44d43c28fdaa
Thulasi <thulasipanndeshwara02@gmail.com>
2025-12-15 12:13:35 +0530

    added X

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css';
 
 const CustomNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar expand="xl" className="custom-navbar" fixed="top">
+    <Navbar
+      expand="xl"
+      className="custom-navbar"
+      fixed="top"
+      expanded={expanded}
+    >
       <Container>
         <Navbar.Brand className="brand-name">Thulasi</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+          className="custom-toggle"
+        >
+          {expanded ? <FaTimes size={22} /> : <FaBars size={22} />}
+        </Navbar.Toggle>
+
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto nav-links">
+          <Nav className="ml-auto nav-links" onClick={() => setExpanded(false)}>
             <Link to="about" smooth={true} duration={600} className="nav-link">About</Link>
             <Link to="education" smooth={true} duration={600} className="nav-link">Education</Link>
             <Link to="skills" smooth={true} duration={600} className="nav-link">Skills</Link>
